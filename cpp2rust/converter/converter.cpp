ddd13a8f6c7f7e9241c2558d5a5a670871c9c890
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-04-13 21:36:20 +0100

    clang-format

--- cpp2rust/converter/converter.cpp
+++ cpp2rust/converter/converter.cpp
@@ -1397,7 +1397,8 @@ void Converter::ConvertGenericCallExpr(clang::CallExpr *expr) {
   StrCat(token::kOpenParen);
   StrCat(keyword_unsafe_);
   StrCat(token::kOpenCurlyBracket);
-  const auto *function = expr->getCalleeDecl() ? expr->getCalleeDecl()->getAsFunction() : nullptr;
+  const auto *function =
+      expr->getCalleeDecl() ? expr->getCalleeDecl()->getAsFunction() : nullptr;
   const clang::FunctionProtoType *proto = nullptr;
 
   if (!function) {
