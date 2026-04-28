d84745a73515bbdebb2afff035b2cf57d97c11f0
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-04-28 10:06:03 +0100

    Update expected output

---
+++ tests/unit/out/refcount/union_flex_array_member.rs
@@ -0,0 +1,73 @@
+extern crate libcc2rs;
+use libcc2rs::*;
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+use std::io::prelude::*;
+use std::io::{Read, Seek, Write};
+use std::os::fd::AsFd;
+use std::rc::{Rc, Weak};
+#[derive()]
+pub struct node_anon_0 {
+    pub bytes: Value<Box<[u8]>>,
+    pub aligner: Value<AnyPtr>,
+}
+impl ByteRepr for node_anon_0 {}
+#[derive(Default)]
+pub struct node {
+    pub len: Value<u64>,
+    pub x: Value<node_anon_0>,
+}
+impl ByteRepr for node {}
+pub fn main() {
+    std::process::exit(main_0());
+}
+fn main_0() -> i32 {
+    let tail_size: Value<u64> = Rc::new(RefCell::new(32_u64));
+    let n: Value<Ptr<node>> = Rc::new(RefCell::new(
+        ({
+            let ___size: u64 =
+                (::std::mem::size_of::<node>() as u64 as u64).wrapping_add((*tail_size.borrow()));
+            malloc_0(___size)
+        })
+        .cast::<node>()
+        .expect("ub:wrong type"),
+    ));
+    (*(*(*n.borrow()).upgrade().deref()).len.borrow_mut()) = (*tail_size.borrow());
+    let i: Value<u64> = Rc::new(RefCell::new(0_u64));
+    'loop_: while ((*i.borrow()) < (*tail_size.borrow())) {
+        let __rhs = ((((*i.borrow()) & 255_u64) as u64) as u8);
+        (*(*(*(*n.borrow()).upgrade().deref()).x.borrow())
+            .bytes
+            .borrow_mut())[(*i.borrow()) as usize] = __rhs;
+        (*i.borrow_mut()).postfix_inc();
+    }
+    let i: Value<u64> = Rc::new(RefCell::new(0_u64));
+    'loop_: while ((*i.borrow()) < (*tail_size.borrow())) {
+        assert!({
+            let _lhs = ((*(*(*(*n.borrow()).upgrade().deref()).x.borrow())
+                .bytes
+                .borrow())[(*i.borrow()) as usize] as i32);
+            _lhs == (((((*i.borrow()) & 255_u64) as u64) as u8) as i32)
+        });
+        (*i.borrow_mut()).postfix_inc();
+    }
+    let p: Value<Ptr<u8>> = Rc::new(RefCell::new(
+        (((*(*(*n.borrow()).upgrade().deref()).x.borrow())
+            .bytes
+            .as_pointer() as Ptr<u8>)
+            .offset(10 as isize)),
+    ));
+    assert!(((((*p.borrow()).read()) as i32) == 10));
+    (*p.borrow()).write(170_u8);
+    assert!(
+        (((*(*(*(*n.borrow()).upgrade().deref()).x.borrow())
+            .bytes
+            .borrow())[(10) as usize] as i32)
+            == 170)
+    );
+    ({
+        let ___ptr: AnyPtr = ((*n.borrow()).clone() as Ptr<node>).to_any();
+        free_1(___ptr)
+    });
+    return 0;
+}
---
+++ tests/unit/out/unsafe/union_struct_dual_use.rs
@@ -0,0 +1,60 @@
+extern crate libc;
+use libc::*;
+extern crate libcc2rs;
+use libcc2rs::*;
+use std::collections::BTreeMap;
+use std::io::{Read, Seek, Write};
+use std::os::fd::{AsFd, FromRawFd, IntoRawFd};
+use std::rc::Rc;
+#[derive(Copy, Clone, Default)]
+pub struct Inner {
+    pub a: i32,
+    pub b: i32,
+}
+pub unsafe fn sum_inner_0(mut i: *mut Inner) -> i32 {
+    return (((*i).a) + ((*i).b));
+}
+#[derive(Copy, Clone)]
+pub struct Outer_anon_0 {
+    pub inner: Inner,
+    pub raw_: [u8; 16],
+}
+#[derive(Copy, Clone, Default)]
+pub struct Outer {
+    pub u: Outer_anon_0,
+}
+pub fn main() {
+    unsafe {
+        std::process::exit(main_0() as i32);
+    }
+}
+unsafe fn main_0() -> i32 {
+    let mut standalone: Inner = <Inner>::default();
+    standalone.a = 3;
+    standalone.b = 4;
+    assert!(
+        ((unsafe {
+            let _i: *mut Inner = (&mut standalone as *mut Inner);
+            sum_inner_0(_i)
+        }) == (7))
+    );
+    let mut outer: Outer = <Outer>::default();
+    {
+        let byte_0 = ((&mut outer as *mut Outer) as *mut Outer as *mut ::libc::c_void) as *mut u8;
+        for offset in 0..::std::mem::size_of::<Outer>() as u64 {
+            *byte_0.offset(offset as isize) = 0 as u8;
+        }
+        ((&mut outer as *mut Outer) as *mut Outer as *mut ::libc::c_void)
+    };
+    outer.u.inner.a = 3;
+    outer.u.inner.b = 4;
+    assert!(
+        ((unsafe {
+            let _i: *mut Inner = (&mut outer.u.inner as *mut Inner);
+            sum_inner_0(_i)
+        }) == (7))
+    );
+    assert!((((outer.u.raw_[(0) as usize] as u8) as i32) == (3)));
+    assert!((((outer.u.raw_[(4) as usize] as u8) as i32) == (4)));
+    return 0;
+}
