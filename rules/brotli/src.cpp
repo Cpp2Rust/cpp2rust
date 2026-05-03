c849c0bad1a420b103d8a5f6fdfa6fd167d35869
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-03 16:25:24 +0100

    Add more brotli rules

--- rules/brotli/src.cpp
+++ rules/brotli/src.cpp
@@ -59,3 +59,35 @@ BrotliDecoderResult f10() { return BROTLI_DECODER_RESULT_ERROR; }
 BrotliDecoderResult f11() { return BROTLI_DECODER_RESULT_NEEDS_MORE_OUTPUT; }
 
 BrotliEncoderMode f12() { return BROTLI_MODE_GENERIC; }
+
+BrotliDecoderResult f13() { return BROTLI_DECODER_RESULT_NEEDS_MORE_INPUT; }
+
+BrotliDecoderErrorCode f14() { return BROTLI_DECODER_ERROR_FORMAT_EXUBERANT_NIBBLE; }
+BrotliDecoderErrorCode f15() { return BROTLI_DECODER_ERROR_FORMAT_EXUBERANT_META_NIBBLE; }
+BrotliDecoderErrorCode f16() { return BROTLI_DECODER_ERROR_FORMAT_SIMPLE_HUFFMAN_ALPHABET; }
+BrotliDecoderErrorCode f17() { return BROTLI_DECODER_ERROR_FORMAT_SIMPLE_HUFFMAN_SAME; }
+BrotliDecoderErrorCode f18() { return BROTLI_DECODER_ERROR_FORMAT_CL_SPACE; }
+BrotliDecoderErrorCode f19() { return BROTLI_DECODER_ERROR_FORMAT_HUFFMAN_SPACE; }
+BrotliDecoderErrorCode f20() { return BROTLI_DECODER_ERROR_FORMAT_CONTEXT_MAP_REPEAT; }
+BrotliDecoderErrorCode f21() { return BROTLI_DECODER_ERROR_FORMAT_BLOCK_LENGTH_1; }
+BrotliDecoderErrorCode f22() { return BROTLI_DECODER_ERROR_FORMAT_BLOCK_LENGTH_2; }
+BrotliDecoderErrorCode f23() { return BROTLI_DECODER_ERROR_FORMAT_TRANSFORM; }
+BrotliDecoderErrorCode f24() { return BROTLI_DECODER_ERROR_FORMAT_DICTIONARY; }
+BrotliDecoderErrorCode f25() { return BROTLI_DECODER_ERROR_FORMAT_WINDOW_BITS; }
+BrotliDecoderErrorCode f26() { return BROTLI_DECODER_ERROR_FORMAT_PADDING_1; }
+BrotliDecoderErrorCode f27() { return BROTLI_DECODER_ERROR_FORMAT_PADDING_2; }
+BrotliDecoderErrorCode f28() { return BROTLI_DECODER_ERROR_INVALID_ARGUMENTS; }
+BrotliDecoderErrorCode f29() { return BROTLI_DECODER_ERROR_ALLOC_CONTEXT_MODES; }
+BrotliDecoderErrorCode f30() { return BROTLI_DECODER_ERROR_ALLOC_TREE_GROUPS; }
+BrotliDecoderErrorCode f31() { return BROTLI_DECODER_ERROR_ALLOC_CONTEXT_MAP; }
+BrotliDecoderErrorCode f32() { return BROTLI_DECODER_ERROR_ALLOC_RING_BUFFER_1; }
+BrotliDecoderErrorCode f33() { return BROTLI_DECODER_ERROR_ALLOC_RING_BUFFER_2; }
+BrotliDecoderErrorCode f34() { return BROTLI_DECODER_ERROR_ALLOC_BLOCK_TYPE_TREES; }
+
+BrotliDecoderErrorCode f35(const BrotliDecoderState *state) {
+  return BrotliDecoderGetErrorCode(state);
+}
+
+uint32_t f36() { return BrotliDecoderVersion(); }
+
+BrotliDecoderErrorCode f37() { return BROTLI_DECODER_ERROR_DICTIONARY_NOT_SET; }
--- rules/brotli/tgt_unsafe.rs
+++ rules/brotli/tgt_unsafe.rs
@@ -79,3 +79,83 @@ unsafe fn f11() -> ::brotli_sys::BrotliDecoderResult {
 unsafe fn f12() -> ::brotli_sys::BrotliEncoderMode {
     ::brotli_sys::BROTLI_MODE_GENERIC
 }
+
+unsafe fn f13() -> ::brotli_sys::BrotliDecoderResult {
+    ::brotli_sys::BROTLI_DECODER_RESULT_NEEDS_MORE_INPUT
+}
+
+unsafe fn f14() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_EXUBERANT_NIBBLE
+}
+unsafe fn f15() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_EXUBERANT_META_NIBBLE
+}
+unsafe fn f16() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_SIMPLE_HUFFMAN_ALPHABET
+}
+unsafe fn f17() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_SIMPLE_HUFFMAN_SAME
+}
+unsafe fn f18() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_CL_SPACE
+}
+unsafe fn f19() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_HUFFMAN_SPACE
+}
+unsafe fn f20() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_CONTEXT_MAP_REPEAT
+}
+unsafe fn f21() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_BLOCK_LENGTH_1
+}
+unsafe fn f22() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_BLOCK_LENGTH_2
+}
+unsafe fn f23() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_TRANSFORM
+}
+unsafe fn f24() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_DICTIONARY
+}
+unsafe fn f25() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_WINDOW_BITS
+}
+unsafe fn f26() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_PADDING_1
+}
+unsafe fn f27() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_FORMAT_PADDING_2
+}
+unsafe fn f28() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_INVALID_ARGUMENTS
+}
+unsafe fn f29() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_ALLOC_CONTEXT_MODES
+}
+unsafe fn f30() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_ALLOC_TREE_GROUPS
+}
+unsafe fn f31() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_ALLOC_CONTEXT_MAP
+}
+unsafe fn f32() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_ALLOC_RING_BUFFER_1
+}
+unsafe fn f33() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_ALLOC_RING_BUFFER_2
+}
+unsafe fn f34() -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BROTLI_DECODER_ERROR_ALLOC_BLOCK_TYPE_TREES
+}
+
+unsafe fn f35(a0: *const ::brotli_sys::BrotliDecoderState) -> ::brotli_sys::BrotliDecoderErrorCode {
+    ::brotli_sys::BrotliDecoderGetErrorCode(a0)
+}
+
+unsafe fn f36() -> u32 {
+    ::brotli_sys::BrotliDecoderVersion()
+}
+
+unsafe fn f37() -> ::brotli_sys::BrotliDecoderErrorCode {
+    -19_i32 as ::brotli_sys::BrotliDecoderErrorCode
+}
