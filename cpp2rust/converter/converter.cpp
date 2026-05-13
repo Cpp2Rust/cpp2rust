6d3df91b3ef26a91193c92d5e1c835d24b2ce553
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-13 10:06:52 +0100

    Revert "Drop integer suffix when visiting integer literal"
    This reverts commit 87b9e363fd5c1852ef09dcb4dfb068d451a8cd82.

--- cpp2rust/converter/converter.cpp
+++ cpp2rust/converter/converter.cpp
@@ -1633,7 +1633,7 @@ bool Converter::VisitIntegerLiteral(clang::IntegerLiteral *expr) {
     computed_expr_type_ = ComputedExprType::FreshValue;
     return false;
   }
-  StrCat(getIntegerLiteral(expr, false));
+  StrCat(getIntegerLiteral(expr, Mapper::Map(expr->getType()) != "i32"));
   computed_expr_type_ = ComputedExprType::FreshValue;
   return false;
 }
