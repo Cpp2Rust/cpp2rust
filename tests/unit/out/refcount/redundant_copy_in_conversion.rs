1bca3ec954625fe25e490a2ec3d3a84165ae108b
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-18 14:14:48 +0100

    Check if test passes on macOS

--- tests/unit/out/refcount/redundant_copy_in_conversion.rs
+++ tests/unit/out/refcount/redundant_copy_in_conversion.rs
@@ -6,6 +6,15 @@ use std::io::prelude::*;
 use std::io::{Read, Seek, Write};
 use std::os::fd::AsFd;
 use std::rc::{Rc, Weak};
+pub fn sink_0(it: RefcountMapIter<i32, i32>) -> i32 {
+    let it: Value<RefcountMapIter<i32, i32>> = Rc::new(RefCell::new(it));
+    let cit: Value<RefcountMapIter<i32, i32>> = Rc::new(RefCell::new((*it.borrow()).clone()));
+    return if (*cit.borrow()) == (*it.borrow()).clone() {
+        (*(*it.borrow()).second().borrow())
+    } else {
+        0
+    };
+}
 pub fn main() {
     std::process::exit(main_0());
 }
@@ -21,12 +30,26 @@ fn main_0() -> i32 {
     let end: Value<RefcountMapIter<i32, i32>> = Rc::new(RefCell::new(RefcountMapIter::end(
         (m.as_pointer() as Ptr<BTreeMap<i32, Value<i32>>>),
     )));
-    let const_it: Value<RefcountMapIter<i32, i32>> = Rc::new(RefCell::new(
-        RefcountMapIter::find_key((m.as_pointer() as Ptr<BTreeMap<i32, Value<i32>>>), &0),
+    let it0: Value<RefcountMapIter<i32, i32>> = Rc::new(RefCell::new(RefcountMapIter::find_key(
+        (m.as_pointer() as Ptr<BTreeMap<i32, Value<i32>>>),
+        &0,
+    )));
+    let const_it: Value<RefcountMapIter<i32, i32>> = Rc::new(RefCell::new((*it0.borrow()).clone()));
+    let r: Value<i32> = Rc::new(RefCell::new(
+        if (*const_it.borrow()) == (*end.borrow()).clone() {
+            0
+        } else {
+            1
+        },
     ));
-    return if (*const_it.borrow()) == (*end.borrow()) {
+    (*r.borrow_mut()) += ({
+        let _it: RefcountMapIter<i32, i32> = (*it0.borrow()).clone();
+        sink_0(_it)
+    });
+    (*r.borrow_mut()) += if (*end.borrow()) == (*end.borrow()) {
         0
     } else {
         1
     };
+    return (*r.borrow());
 }
