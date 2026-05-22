9cec5b60571b8e7e8f12dc424fdae79ca6270504
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-22 14:24:17 +0100

    Add IPPROTO IPV6 and MPTCP

--- rules/ip/src.cpp
+++ rules/ip/src.cpp
@@ -11,3 +11,11 @@ int f2() {
 int f3() {
   return IPPROTO_IP;
 }
+
+int f4() {
+  return IPPROTO_IPV6;
+}
+
+int f5() {
+  return IPPROTO_MPTCP;
+}
--- rules/ip/tgt_unsafe.rs
+++ rules/ip/tgt_unsafe.rs
@@ -9,3 +9,11 @@ unsafe fn f2() -> i32 {
 unsafe fn f3() -> i32 {
     libc::IPPROTO_IP
 }
+
+unsafe fn f4() -> i32 {
+    libc::IPPROTO_IPV6
+}
+
+unsafe fn f5() -> i32 {
+    libc::IPPROTO_MPTCP
+}
