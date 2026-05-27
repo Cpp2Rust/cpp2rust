a1e16d79b8d7a437d1bc9f4eab36d4a41b8678dc
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-05-27 11:34:50 +0100

    Add rules for fd_set macros (#150)

---
+++ tests/unit/fd_set_macros.c
@@ -0,0 +1,14 @@
+// no-compile: refcount
+#include <assert.h>
+#include <sys/select.h>
+
+int main() {
+  fd_set set;
+  FD_ZERO(&set);
+  assert(!FD_ISSET(3, &set));
+  FD_SET(3, &set);
+  assert(FD_ISSET(3, &set));
+  FD_CLR(3, &set);
+  assert(!FD_ISSET(3, &set));
+  return 0;
+}
