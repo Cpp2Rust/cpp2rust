221ee449d09b28e7d2e43b002d79bffb56c20064
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2026-08-19 11:25:18 +0100

    Add a test to ensure no empty `thread_local`s are being generated

---
+++ tests/unit/extern_global.c
@@ -0,0 +1,4 @@
+extern int x;
+int x;
+
+int main(void) { return x; }
---
+++ tests/unit/out/refcount/extern_global.rs
@@ -0,0 +1,17 @@
+extern crate libcc2rs;
+use libcc2rs::*;
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+use std::io::prelude::*;
+use std::io::{Read, Seek, Write};
+use std::os::fd::AsFd;
+use std::rc::{Rc, Weak};
+thread_local!(
+    pub static x_0: Value<i32> = <Value<i32>>::default();
+);
+pub fn main() {
+    std::process::exit(main_0());
+}
+fn main_0() -> i32 {
+    return (*x_0.with(Value::clone).borrow());
+}
