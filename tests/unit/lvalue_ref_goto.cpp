2c6ac2551d5ef6b8d9eeefeeb7de16bfdac08563
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2026-08-26 21:31:47 +0000

    Add more pre-commit tests

---
+++ tests/unit/lvalue_ref_goto.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022-present INESC-ID.
+// Distributed under the MIT license that can be found in the LICENSE file.
+
+#include <cassert>
+
+int main() {
+  const int &r = 5;
+  goto body;
+body:
+  assert(r == 5);
+  return 0;
+}
