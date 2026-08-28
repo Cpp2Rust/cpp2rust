869b92b09d6a8a40a318fa8932cfaef13277f1b0
João Gouveia <joaotalonegouveia@tecnico.ulisboa.pt>
2026-08-28 15:09:45 +0000

    Address reviews

--- cpp2rust/converter/models/converter_refcount.cpp
+++ cpp2rust/converter/models/converter_refcount.cpp
@@ -368,8 +368,7 @@ ConverterRefCount::MaterializeTemp(const std::string &binding_name,
   auto pointee = param_type.getNonReferenceType();
   auto value = ConvertRValue(expr, pointee);
   auto type_str = ToStringBase(pointee);
-  auto decl = in_const_initializer_ ? std::string("pub ") + keyword::kStatic
-                                    : keyword::kLet;
+  const char *decl = in_const_initializer_ ? "pub static" : keyword::kLet;
 
   auto binding = std::format("{} {} : Value<{}> = Rc::new(RefCell::new({}));",
                              decl, binding_name, type_str, value);
