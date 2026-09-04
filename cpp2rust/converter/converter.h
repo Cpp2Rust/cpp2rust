18a5c4afdd7d244b832bb5e7e2dd519ef220f26a
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-04 18:52:18 +0100

    Translate enums as integres

--- cpp2rust/converter/converter.h
+++ cpp2rust/converter/converter.h
@@ -374,9 +374,7 @@ public:
 
   virtual bool VisitEnumDecl(clang::EnumDecl *decl);
 
-  virtual void AddFromImpl(clang::EnumDecl *decl);
-
-  virtual void AddIncDecImpls(clang::EnumDecl *decl);
+  virtual std::string EnumeratorName(const clang::EnumConstantDecl *decl) const;
 
   virtual bool VisitCXXDefaultArgExpr(clang::CXXDefaultArgExpr *expr);
 
@@ -560,7 +558,6 @@ protected:
 
   virtual void AddByteReprTrait(const clang::RecordDecl *decl);
 
-  virtual void AddByteReprTrait(const clang::EnumDecl *decl);
 
   virtual void
   ConvertUnsignedArithBinaryOperator(clang::BinaryOperator *binary_operator,
