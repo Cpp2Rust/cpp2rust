5c99fd1a9a93e3becb142a2b533d2682bb5fb916
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-04 20:17:15 +0100

    Allow records with system header fields to implement ByteRepr (#300)

--- tests/unit/out/refcount/void_cast.rs
+++ tests/unit/out/refcount/void_cast.rs
@@ -22,7 +22,19 @@ impl Clone for NonTrivial {
         this
     }
 }
-impl ByteRepr for NonTrivial {}
+impl ByteRepr for NonTrivial {
+    fn byte_size() -> usize {
+        24
+    }
+    fn to_bytes(&self, buf: &mut [u8]) {
+        (*self.data.borrow()).to_bytes(&mut buf[0..24]);
+    }
+    fn from_bytes(buf: &[u8]) -> Self {
+        Self {
+            data: Rc::new(RefCell::new(<Vec<i32>>::from_bytes(&buf[0..24]))),
+        }
+    }
+}
 pub fn unused_ref_param_1(x: Ptr<NonTrivial>) {
     (*x.upgrade().deref()).clone();
 }
