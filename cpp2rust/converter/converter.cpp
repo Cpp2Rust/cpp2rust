092c0aac59d07e6461ce45c82feb1899ca217b5c
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-09 10:16:30 +0100

    Call take on unique_ptr in unsafe

--- cpp2rust/converter/converter.cpp
+++ cpp2rust/converter/converter.cpp
@@ -1718,6 +1718,11 @@ bool Converter::VisitCallExpr(clang::CallExpr *expr) {
   }
 
   if (expr->isCallToStdMove()) {
+    if (IsUniquePtr(expr->getArg(0)->getType())) {
+      StrCat(std::format("{}.take()", ConvertLValue(expr->getArg(0))));
+      computed_expr_type_ = ComputedExprType::FreshValue;
+      return false;
+    }
     StrCat(std::format("{}", ToString(expr->getArg(0))));
     computed_expr_type_ = ComputedExprType::FreshValue;
     return false;
