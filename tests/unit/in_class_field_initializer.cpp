f363de31193dcc02da2d3a97cc66452b5eac2b64
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-10 15:53:26 +0100

    Add template specialization with in-class initializer test

--- tests/unit/in_class_field_initializer.cpp
+++ tests/unit/in_class_field_initializer.cpp
@@ -13,6 +13,12 @@ struct S {
   Inner d;
 };
 
+template <typename T> struct Boxed {
+  T v = T();
+  int tag = 7;
+  Boxed(T x, int t) : v(x), tag(t) {}
+};
+
 int main() {
   S s;
   assert(s.a == 1);
@@ -21,5 +27,8 @@ int main() {
   assert(s.c.y == 4);
   assert(s.d.x == 3);
   assert(s.d.y == 4);
+  Boxed<int> boxed(5, 9);
+  assert(boxed.v == 5);
+  assert(boxed.tag == 9);
   return 0;
 };
