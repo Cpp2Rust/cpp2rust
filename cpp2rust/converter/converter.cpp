bb31eddc9535fce6e2106f8ae2bff0f46a7ac997
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-12 14:30:47 +0100

    Fix unnamed parameters in comprison operators

--- cpp2rust/converter/converter.cpp
+++ cpp2rust/converter/converter.cpp
@@ -1848,7 +1848,7 @@ Converter::CallInfo Converter::CollectCallInfo(clang::CallExpr *expr) {
   for (unsigned i = 0; i < num_named_params && i < num_args; ++i) {
     auto *arg = expr->getArg(i + arg_begin);
     CallArg ca{
-        .param_name = function
+        .param_name = function && !function->getParamDecl(i)->getName().empty()
                           ? ("_" + function->getParamDecl(i)->getNameAsString())
                           : ("_arg" + std::to_string(i)),
         .param_type = function ? function->getParamDecl(i)->getType()
--- cpp2rust/converter/converter_lib.cpp
+++ cpp2rust/converter/converter_lib.cpp
@@ -648,12 +648,17 @@ std::string GetNamedDeclAsString(const clang::NamedDecl *decl) {
     auto *pdecl = llvm::dyn_cast<clang::ParmVarDecl>(decl);
     assert(pdecl && "Unexpected unnamed construct");
 
-    const auto *ctor =
-        llvm::dyn_cast<clang::CXXConstructorDecl>(pdecl->getDeclContext());
-    name = (pdecl->isExplicitObjectParameter() ||
-            (ctor && ctor->isCopyOrMoveConstructor()))
-               ? "self"
-               : "_";
+    const auto *fn =
+        llvm::dyn_cast<clang::FunctionDecl>(pdecl->getDeclContext());
+    const auto *ctor = llvm::dyn_cast_or_null<clang::CXXConstructorDecl>(fn);
+    if (pdecl->isExplicitObjectParameter() ||
+        (ctor && ctor->isCopyOrMoveConstructor())) {
+      name = "self";
+    } else if (fn && fn->isDefaulted() && IsComparisonOperator(fn)) {
+      name = std::format("_arg{}", pdecl->getFunctionScopeIndex());
+    } else {
+      name = "_";
+    }
   } else if (auto *pdecl = llvm::dyn_cast<clang::ParmVarDecl>(decl)) {
     // Expanded parameter packs share one name across the expansion
     if (auto *fn = llvm::dyn_cast_or_null<clang::FunctionDecl>(
