722e11174450abbafff3a39cc5d01ad6c3c5ac93
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-20 13:54:26 +0100

    Translate unary plus

--- cpp2rust/converter/converter.cpp
+++ cpp2rust/converter/converter.cpp
@@ -2876,6 +2876,7 @@ bool Converter::VisitUnaryOperator(clang::UnaryOperator *expr) {
   }
   switch (opcode) {
   case clang::UO_Extension:
+  case clang::UO_Plus:
     Convert(sub_expr);
     break;
   case clang::UO_AddrOf: {
