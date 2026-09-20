e6e3bd689f0bc04d75cb47a4d25aeb24eae9d78f
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-20 12:55:02 +0100

    Convert delegating constructors (#386)

--- tests/unit/out/refcount/constructor.rs
+++ tests/unit/out/refcount/constructor.rs
@@ -47,6 +47,68 @@ impl ByteRepr for S {
         }
     }
 }
+#[derive()]
+pub struct Point {
+    pub x: Value<i32>,
+    pub y: Value<i32>,
+}
+impl Point {
+    pub fn Point1(x: i32, y: i32) -> Self {
+        let x: Value<i32> = Rc::new(RefCell::new(x));
+        let y: Value<i32> = Rc::new(RefCell::new(y));
+        let __this: Value<Point> = Rc::new(RefCell::new(Self {
+            x: Rc::new(RefCell::new((*x.borrow()))),
+            y: Rc::new(RefCell::new((*y.borrow()))),
+        }));
+        let this: Ptr<Point> = __this.as_pointer();
+        Rc::try_unwrap(__this).ok().unwrap().into_inner()
+    }
+    pub fn Point2(v: i32) -> Self {
+        let v: Value<i32> = Rc::new(RefCell::new(v));
+        let __this: Value<Point> = Rc::new(RefCell::new(Point::Point1({ (*v.borrow()) }, {
+            ((*v.borrow()) + 1)
+        })));
+        let this: Ptr<Point> = __this.as_pointer();
+        (*(*this.upgrade().deref()).y.borrow_mut()) *= 10;
+        Rc::try_unwrap(__this).ok().unwrap().into_inner()
+    }
+    pub fn Point3() -> Self {
+        let __this: Value<Point> = Rc::new(RefCell::new(Point::Point2({ 4 })));
+        let this: Ptr<Point> = __this.as_pointer();
+        (*(*this.upgrade().deref()).x.borrow_mut()) += 100;
+        Rc::try_unwrap(__this).ok().unwrap().into_inner()
+    }
+}
+impl Clone for Point {
+    fn clone(&self) -> Self {
+        let __this: Value<Point> = Rc::new(RefCell::new(Self {
+            x: Rc::new(RefCell::new((*self.x.borrow()))),
+            y: Rc::new(RefCell::new((*self.y.borrow()))),
+        }));
+        let this: Ptr<Point> = __this.as_pointer();
+        Rc::try_unwrap(__this).ok().unwrap().into_inner()
+    }
+}
+impl Default for Point {
+    fn default() -> Self {
+        { Point::Point3() }
+    }
+}
+impl ByteRepr for Point {
+    fn byte_size() -> usize {
+        8
+    }
+    fn to_bytes(&self, buf: &mut [u8]) {
+        (*self.x.borrow()).to_bytes(&mut buf[0..4]);
+        (*self.y.borrow()).to_bytes(&mut buf[4..8]);
+    }
+    fn from_bytes(buf: &[u8]) -> Self {
+        Self {
+            x: Rc::new(RefCell::new(<i32>::from_bytes(&buf[0..4]))),
+            y: Rc::new(RefCell::new(<i32>::from_bytes(&buf[4..8]))),
+        }
+    }
+}
 pub fn main() {
     __cpp2rust_init_globals();
     std::process::exit(main_0());
@@ -59,6 +121,12 @@ fn main_0() -> i32 {
         assert!((total_0.with(|rc| *rc.borrow()) == 8));
     }
     assert!((total_0.with(|rc| *rc.borrow()) == 18));
+    let p: Value<Point> = Rc::new(RefCell::new(Point::Point3()));
+    assert!(((*(*p.borrow()).x.borrow()) == 104));
+    assert!(((*(*p.borrow()).y.borrow()) == 50));
+    let q: Value<Point> = Rc::new(RefCell::new(Point::Point2({ 7 })));
+    assert!(((*(*q.borrow()).x.borrow()) == 7));
+    assert!(((*(*q.borrow()).y.borrow()) == 80));
     return 0;
 }
 pub trait SImpl {
