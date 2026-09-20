6f73b26a535ef09e84eb601c2e8a039caf0c28c0
Lucian Popescu <lucian.popescu@tecnico.ulisboa.pt>
2026-09-20 14:13:27 +0100

    Define constexpr cpp2rust_assert_fail

--- cpp2rust/compat/assert.h
+++ cpp2rust/compat/assert.h
@@ -5,10 +5,18 @@
 
 #undef assert
 
-#ifndef __cplusplus
+#ifdef __cplusplus
+#ifndef CPP2RUST_ASSERT_FAIL_DEFINED
+#define CPP2RUST_ASSERT_FAIL_DEFINED
+constexpr void cpp2rust_assert_fail(bool condition) {
+  if (!condition) {
+    __builtin_trap();
+  }
+}
+#endif // CPP2RUST_ASSERT_FAIL_DEFINED
+#define assert(expr) cpp2rust_assert_fail(static_cast<bool>(expr))
+#else
 #include <stdbool.h>
-#endif
-
 void cpp2rust_assert_fail(bool condition) __attribute__((noreturn));
-
 #define assert(expr) cpp2rust_assert_fail(expr)
+#endif // __cplusplus
